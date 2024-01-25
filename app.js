215e44e200ae5bcdb467d4d4b16db29fd731aed5
Ibrahim <abdulmumeenibroheem51@gmail.com>
2024-01-25 13:13:12 +0100

    chart-setup..2

--- app.js
+++ app.js
@@ -2,23 +2,23 @@ const myChart = new Chart("canvas", {
   type: "bar",
   data: {
     labels: [
-      "jan",
-      "feb",
-      "mar",
+      "Jan",
+      "Feb",
+      "Mar",
       "Apr",
       "Mei",
-      "jun",
-      "jul",
-      "aug",
-      "sep",
-      "okt",
-      "nov",
-      "dec",
+      "Jun",
+      "Jul",
+      "Aug",
+      "Sep",
+      "Okt",
+      "Nov",
+      "Des",
     ],
     datasets: [
       {
         label: "Chartter",
-        data: [65, 59, 80, 81, 56, 55, 40, 34, 21, 45, 12, 5, 67],
+        data: [6.0, 18.0, 4.0, 28.0, 9.0, 45.0, 19.0, 22, 32, 4.8, 30, 27],
         backgroundColor: [
           "rgba(255, 99, 132, 0.1)",
           "rgba(255, 159, 64, 0.2)",
@@ -29,16 +29,50 @@ const myChart = new Chart("canvas", {
           "rgba(201, 203, 207, 0.2)",
         ],
         borderColor: ["transparent"],
-        borderWidth: 2,
-        borderRadius: 50,
       },
     ],
   },
   options: {
+    legend: { display: false },
     scales: {
-      y: {
-        beginAtZero: true,
-      },
+      xAxes: [
+        {
+          ticks: {
+            fontColor: "#525252",
+            fontFamily: "Plus Jakarta Sans",
+            fontSize: 12,
+          },
+          gridLines: {
+            display: false,
+            drawBorder: false,
+          },
+        },
+      ],
+      yAxes: [
+        {
+          ticks: {
+            min: 0,
+            max: 50,
+            stepSize: 10, // Set the desired step size
+            callback: function (value) {
+              return value === 0
+                ? "0"
+                : value.toLocaleString("en-US", {
+                    minimumFractionDigits: 3,
+                  });
+            },
+            maxTicksLimit: 6,
+            precision: 2,
+            fontColor: "#525252",
+            fontFamily: "Plus Jakarta Sans",
+            fontSize: 12,
+          },
+
+          gridLines: {
+            borderDash: [5, 5], // Set the x-axis grid lines to dashes
+          },
+        },
+      ],
     },
   },
 });
